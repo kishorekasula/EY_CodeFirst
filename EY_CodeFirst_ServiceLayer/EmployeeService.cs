67c5760cbb7b8a34ae31362300310260dd8c937f
kishorekasula <kishorekasula123@gmail.com>
2024-12-14 15:08:36 +0530

    First Commit

--- EY_CodeFirst_ServiceLayer/EmployeeService.cs
+++ EY_CodeFirst_ServiceLayer/EmployeeService.cs
@@ -16,7 +16,7 @@ namespace EY_CodeFirst_ServiceLayer
         public EmployeeService(IEmployeeRepository employeeRepository)
         {
             //Obj creation
-            this._employeeRepository = employeeRepository;  
+            this._employeeRepository = employeeRepository;
         }
         public bool AddEmployeeDetails(EmployeeDTO employeeDTO)
         {
@@ -44,19 +44,27 @@ namespace EY_CodeFirst_ServiceLayer
             List<EmployeeDTO> employeeDTOobj = new List<EmployeeDTO>();
 
             List<Employee> employees = _employeeRepository.GetAllEmployeeDetails();
-            foreach (Employee employee in employees)
+
+            if (employees == null)
             {
-                EmployeeDTO obj = new EmployeeDTO();
-                obj.ID = employee.ID;
-                obj.FirstName = employee.FirstName;
-                obj.LastName = employee.LastName;
-                obj.Email = employee.Email;
-                obj.Phone = employee.Phone;
-                obj.Address = employee.Address;
-                obj.Gender = employee.Gender;
-                employeeDTOobj.Add(obj);
+                return employeeDTOobj;
             }
-            return employeeDTOobj;
+            else
+            {
+                foreach (Employee employee in employees)
+                {
+                    EmployeeDTO obj = new EmployeeDTO();
+                    obj.ID = employee.ID;
+                    obj.FirstName = employee.FirstName;
+                    obj.LastName = employee.LastName;
+                    obj.Email = employee.Email;
+                    obj.Phone = employee.Phone;
+                    obj.Address = employee.Address;
+                    obj.Gender = employee.Gender;
+                    employeeDTOobj.Add(obj);
+                }
+                return employeeDTOobj;
+            }
         }
 
         public List<EmployeeDTO> GetEmployeeDetailsByID(int id)
